7feb1c517c952e580b6e4ebb9d62fd46cec99ec2
runew <rune.willen@outlook.dk>
2026-01-30 08:35:52 +0800

    Added HelloWorld.c

--- 01_BasicsOfC/HelloWorld/HelloWorld.c
+++ 01_BasicsOfC/HelloWorld/HelloWorld.c
@@ -1,1 +1,15 @@
-// Test file
+// --------------------- File Inforation ---------------------
+// File			: HelloWorld.c
+// Author		: Rune Dirk Willen
+// Date			: 29-01-2026
+// Description	: A simple Hello World program in C.
+// -----------------------------------------------------------
+
+// Include necessary headers and namespaces
+#include <stdio.h>
+
+// Main function
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
