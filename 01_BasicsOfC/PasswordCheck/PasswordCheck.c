2706048f99356b4e20c4b88d8da35ce88eae5fe5
runew <rune.willen@outlook.dk>
2026-01-30 11:06:21 +0800

    Added PasswordCheck.c

---
+++ 01_BasicsOfC/PasswordCheck/PasswordCheck.c
@@ -0,0 +1,33 @@
+// --------------------- File Inforation ---------------------
+// File			: PasswordCheck.c
+// Author		: Rune Dirk Willen
+// Date			: 30-01-2026
+// Description	: A simple program to check password in C.
+// -----------------------------------------------------------
+
+// Include necessary headers and namespaces
+#include <stdio.h>
+#include <string.h>
+
+// Main function
+int main() {
+	
+	char password[] = "Cake123";
+	int isTrue = 0;
+	char input[100];
+
+	while (isTrue == 0) {
+		printf("Please type your password: ");
+		scanf_s("%s", input, 100);
+
+		if (strcmp(password, input) == 0) {
+			isTrue = 1;
+		}
+		else {
+			printf("Password is wrong! Try again.\n");
+		}
+
+	}
+
+	return 0;
+}
